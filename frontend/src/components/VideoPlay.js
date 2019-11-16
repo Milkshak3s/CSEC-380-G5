6a36e5c3a2f1511fd59b8e3425f2782fb08eacd3
Milkshak3s <justchris.vantine@gmail.com>
2019-11-16 15:40:43 -0500

    Add display table

---
+++ frontend/src/components/VideoPlay.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = theme => ({
+  nothing: {
+    padding: "0px",
+  },
+});
+
+
+class VideoPlay extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
+}
+
+
+export default withStyles(styles)(VideoPlay);
