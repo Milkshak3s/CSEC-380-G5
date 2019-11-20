0075b252d266e901ee842f570ea02305925a1045
Sunggwan Choi <sxc2679@rit.edu>
2019-11-20 00:27:00 -0500

    Added test and docs for cmd injection

---
+++ tests/test_cmdinjection.py
@@ -0,0 +1,20 @@
+import requests 
+import pytest
+
+def test_cmdinjection(url, cmd):
+    data = {'cmd': cmd}
+    #print("[DEBUG] Sending: ", data)
+    r = requests.post(url=url, json=data)
+
+    assert "Linux" in r.text
+    assert 200 == r.status_code
+
+    #print(r.text)
+
+def main():
+    url = "http://127.0.0.1:5000/api/v1/cmdinjection"
+
+    test_cmdinjection(url, '8.8.8.8; cat /etc/os-release')
+
+if __name__ == '__main__':
+    main()
